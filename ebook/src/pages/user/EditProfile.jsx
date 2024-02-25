b00c667746a1d08fa8533e3548aa3d90e2b7fdea
SiddaharthaPaudel <psiddhartha62@gmail.com>
2024-02-25 14:41:57 +0545

    Changes in edit profile

--- ebook/src/pages/user/EditProfile.jsx
+++ ebook/src/pages/user/EditProfile.jsx
@@ -5,7 +5,17 @@ import bookService from "../../service/book.service";
 import { setCurrentUser } from "../../store/action/user.action";
 
 const EditProfile = () => {
- 
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    email: "",
+    password: "",
+    mobNo: "",
+    address: "",
+    city: "",
+    state: "",
+    pincode: "",
+  });
 
   const loginUser = useSelector((state) => state.user);
   const [msg, setMsg] = useState(true);
