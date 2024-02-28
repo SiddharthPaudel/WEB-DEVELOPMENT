6e8bb9e89184aac9eb207d5f47ad56f56c3b7079
SiddaharthaPaudel <psiddhartha62@gmail.com>
2024-02-28 19:17:12 +0545

    Color

--- ebook/src/component/Navbar/Navbar.js
+++ ebook/src/component/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
         <div >
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <Link to="/" className="navbar-brand text-white"><i className="fa-solid fa-book" /> Ebook Store</Link>
+                    <Link to="/" className="navbar-brand text-white"><i className="fa-solid fa-book" /> BookShelf</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
